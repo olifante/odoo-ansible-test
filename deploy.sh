18764159f9a5194e00ae106a497a618b94c10855
Tiago Henriques <trinosauro@gmail.com>
2016-10-10 17:56:22 +0200

    Add alternative commented examples

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
 ansible-playbook playbook.yml -vvvv
+#ansible-playbook playbook.yml -i inventory --private-key=~/.ssh/digitalocean_rsa -vvvv
+#ansible-playbook -i inventory playbook.yml -e "ansible_ssh_user=root" -k --private-key=~/.ssh/digitalocean_rsa
+#ansible-playbook -i inventory playbook.yml -k
--- ping.sh
+++ ping.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 ansible all -m ping -vvvv
+# ansible all -u root --private-key ~/.ssh/digitalocean_rsa -m ping -vvvv
